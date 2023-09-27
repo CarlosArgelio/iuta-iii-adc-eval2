75d30b13e30753fee01bb825829ce032951c0f3f
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-09-27 16:13:12 -0400

    feat: add styles principal project

---
+++ src/styles/styles.principal.js
@@ -0,0 +1,9 @@
+const styles = {
+    principal: '#E6621F',
+    primer_complementary: '#EDEDEE',
+    secundary_complementary: '#898989',
+    color_dark: '#333333',
+    color_white: '#FFFFFF'
+}
+
+export default styles;
