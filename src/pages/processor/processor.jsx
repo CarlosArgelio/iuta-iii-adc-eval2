71d415d62816a69dca28361d155497719d3a68bc
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-09-28 17:30:35 -0400

    fix: change names

--- src/pages/processor/processor.jsx
+++ src/pages/processor/processor.jsx
@@ -2,9 +2,9 @@ import Transition from "./components/transitions/transitions";
 import Cards from "./components/cards/cards";
 
 // images import
-import processador from '../../assets/procesador.webp'
+import processadorImage from '../../assets/procesador.webp'
 
-const bus = () => {
+const Processador = () => {
 
     return (
         <div>
@@ -23,16 +23,16 @@ const bus = () => {
                 <Cards 
                     title={'Procesadores de propósito'} 
                     info={'Son los más comunes. Se utilizan en una amplia gama de dispositivos, desde ordenadores personales hasta teléfonos inteligentes.'} 
-                    image={processador}
+                    image={processadorImage}
                     />
                 <Cards 
                     title={'Procesadores de propósito específico'} 
                     info={'Se diseñan para una tarea específica, como el procesamiento de imágenes o el reconocimiento de voz.'} 
-                    image={processador}
+                    image={processadorImage}
                     />
             </div>
         </div>
   );
 };
 
-export default bus;
+export default Processador;
