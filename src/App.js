5f6129993d93eeabe3ebb336350538c57ee7e982
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-09-30 23:59:40 -0400

    add intro and conclu

--- src/App.js
+++ src/App.js
@@ -3,7 +3,41 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <p>Introduccion</p>
+      <div>
+        <p>Los ordenadores son máquinas complejas que necesitan de una serie de componentes para funcionar. En este tema, se abordarán los siguientes puntos de forma breve:</p>
+
+        <ul>
+          <li>
+            <strong>Bus de datos: </strong> Un bus de datos es un conjunto de cables que conectan la memoria, el procesador y otros dispositivos. Permite que los datos se transmitan entre estos componentes.
+          </li>
+          <li>
+            <strong>Datos y sus tipos: </strong> os datos son la información que se procesa en un ordenador. Pueden ser de diferentes tipos, como numéricos, alfabéticos o de carácter.
+          </li>
+          <li>
+            <strong>Disco: </strong> Un disco es un dispositivo de almacenamiento que permite guardar datos de forma permanente. Los discos duros son los discos más comunes en los ordenadores.
+          </li>
+          <li>
+            <strong>Memoria: </strong> La memoria es el lugar donde se almacenan los datos que se están utilizando en un momento dado. Los tipos de memoria más comunes son la memoria RAM y la memoria ROM.
+          </li>
+          <li>
+            <strong>Procesador: </strong> El procesador es el cerebro del ordenador. Se encarga de interpretar y ejecutar las instrucciones que le envía el software.
+          </li>
+          <li>
+            <strong>Sistemas operativos: </strong> Un sistema operativo es un software que gestiona los recursos de un ordenador. Permite que el usuario pueda interactuar con el ordenador y que las aplicaciones funcionen correctamente.
+          </li>
+        </ul>
+      </div>
+
+      <p>Conclusiones</p>
+      <div>
+        <p>
+          Los componentes mencionados anteriormente son esenciales para el funcionamiento de un ordenador. Cada uno de ellos cumple una función específica que permite que el ordenador pueda realizar tareas.
+        </p>
+        <p>
+          Es importante tener un conocimiento básico de estos componentes para poder comprender cómo funciona un ordenador y cómo utilizarlo de forma eficiente.
+        </p>
+      </div>
     </div>
   );
 }
